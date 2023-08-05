a10dc87b7e7ae4fb5b7d9dd7a87a7e8e476146e4
stealthmachines <stealthmachines@gmail.com>
2023-08-04 22:14:37 -0600

    Update deploy_bot.sh

--- deploy_bot.sh
+++ deploy_bot.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 # Install Docker and its dependencies
+echo "Installing Docker and its dependencies..."
 sudo yum update -y
 sudo yum install -y epel-release
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo yum install -y --allowerasing docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
 
-# Install Python and its dependencies
-sudo yum install -y python3 python3-pip
-sudo yum groupinstall -y "Development Tools"
-sudo yum install -y python3-devel
-sudo pip3 install --upgrade pip
-sudo pip3 install scikit-learn openai
-
 # Create Dockerfile
 echo "Creating Dockerfile..."
-echo "
+cat <<EOF > Dockerfile
 # Use a base image with Python and necessary dependencies
 FROM python:3.8-slim-buster
 
@@ -36,22 +30,23 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY self_improving_bot.py .
 
 # Run the bot's script when the container starts
-CMD [\"python\", \"self_improving_bot.py\"]
-" > Dockerfile
+CMD ["python", "self_improving_bot.py"]
+EOF
 
 # Create requirements.txt
 echo "Creating requirements.txt..."
 echo "
 scikit-learn
 numpy
+openai
 " > requirements.txt
 
 # Build Docker container
 echo "Building Docker container..."
 docker build -t self_improving_bot -f Dockerfile .
 
-# Run Docker container with internet access
+# Run Docker container
 echo "Starting Docker container..."
-docker run -it --rm --network host --privileged --name self_improving_bot_container self_improving_bot
+docker run -it --rm --name self_improving_bot_container self_improving_bot
 
 echo "Script completed."
